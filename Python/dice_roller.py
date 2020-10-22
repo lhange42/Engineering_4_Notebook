4e921b3112a78a5007920d16f15549e00da20bd0
lhange42 <55252149+lhange42@users.noreply.github.com>
2020-10-22 10:00:34 -0400

    Update dice_roller.py

--- Python/dice_roller.py
+++ Python/dice_roller.py
@@ -3,13 +3,13 @@
 
 from random import randint
 
-print ("Automatic Dice Roller")
-go = True
+print ("Automatic Dice Roller") 
+go = True # Makes the condition of the while wloop true allowing it to run
 
 while go:
-	i = str(input("Click enter to roll dice and x to leave:"))
+	i = str(input("Click enter to roll dice and x to leave:")) # prints this message after every dice roll
 	if i == "":
-		print(randint(1,6))
+		print(randint(1,6)) # This prints a random int from 1 to 6
 	elif i == "x":
 		print("exiting apllication")
-		go = False
+		go = False # this else if reads if x then enter was clicked so then it makes go false and stops the loop
