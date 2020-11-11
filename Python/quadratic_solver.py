fb95c01f106b111a412c5f0fd1aa6ceb6a8f9a50
Lukas <lhange42@charlottesvilleschools.org>
2020-11-11 17:02:56 +0000

    commented on my quadratic code

--- Python/quadratic_solver.py
+++ Python/quadratic_solver.py
@@ -1,21 +1,18 @@
 def rootFinder(a,b,c):
-    discriminant = b**2 - 4*a*c
-    roots = []
+    discriminant = b**2 - 4*a*c #determines if the inside of the quadratics square root is positive or negative determining if the roots are real
+    roots = []# creates the array
     
-    if discriminant < 0:
-        return str("There are no real roots")
-    elif discriminant >= 0:
-        root1 = (-b + discriminant**.5)/(2*a)
-        root2 = (-b - discriminant**.5)/(2*a)
-        roots.append(root1)
-        roots.append(root2)
-        return roots
+    if discriminant < 0:# if the discriminant is negative
+        roots.append("There are no real roots")#makes the list contain a string  
+    elif discriminant >= 0:# reads if the discriminant is positive
+        root1 = (-b + discriminant**.5)/(2*a)# solves for the root in which the top of the quadratic is adding
+        root2 = (-b - discriminant**.5)/(2*a)# solves for the root in which the top of the quadratic is subtracting
+        roots.append(root1)# adds the first root to the array
+        roots.append(root2)# adds the second root to the array
+    return roots# returns the roots array to where the rootFinder function was called
 
-
-
-a = int(input("enter the coefficient of your x^2 term: "))
+a = int(input("enter the coefficient of your x^2 term: "))# These variables provide 3 user integer inputs that will become the coefficients of the quadratics
 b = int(input("enter the coefficient of your x term: "))
 c = int(input("enter the value of your numerical term: "))
 
-
-print(rootFinder(a,b,c))
+print(rootFinder(a,b,c))# prints the outcome of the root function as an array 
