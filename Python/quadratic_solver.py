2ef8fe33325ec8e9f6e6b8fc3e6ce23b559e8eac
Lukas <lhange42@charlottesvilleschools.org>
2020-11-11 16:30:39 +0000

    I made a change to my quadratic solver game

--- Python/quadratic_solver.py
+++ Python/quadratic_solver.py
@@ -1,4 +1,3 @@
-
 def rootFinder(a,b,c):
     discriminant = b**2 - 4*a*c
     roots = []
@@ -6,11 +5,12 @@ def rootFinder(a,b,c):
     if discriminant < 0:
         return str("There are no real roots")
     elif discriminant >= 0:
-        root1 = (-b + discriminant**.5)/2*a
-        root2 = (-b - discriminant**.5)/2*a
+        root1 = (-b + discriminant**.5)/(2*a)
+        root2 = (-b - discriminant**.5)/(2*a)
         roots.append(root1)
         roots.append(root2)
         return roots
+
 
 
 a = int(input("enter the coefficient of your x^2 term: "))
