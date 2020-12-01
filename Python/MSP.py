e3d3e6ad548dc4c966e21461733790ea547077b9
Lukas <lhange42@charlottesvilleschools.org>
2020-12-01 19:02:40 +0000

    altered code

--- Python/MSP.py
+++ Python/MSP.py
@@ -4,40 +4,49 @@
 programFinished = False
 guessLetter = ""
 guessList = []
-word = str(input(" What's the word, player 1: ").lower())
+word = str(input(" What's the word, player 1: ").upper())
 word = list(word)
 print(word)
 print ("\n" * 50)
 for i in range(0, len(word)):
     guessList.append("_")
+guessLetter = str(input("\nPlayer 2, guess a letter: ").upper())
+sameGuess = []
+guessNum = 7
 
-while guessList != word:
-    guessLetter = str(input("\nPlayer 2, guess a letter: ").lower())
-    correct = False
-    same = False
 
-    for t in guessList:
-        if guessLetter == t:
-            same = True
+def draw(n):
+    f = open("graham.txt", "r")
+    for x in range(0, (5-n) * 4 + 1):
+        print(f.readline(), end="")
 
-    for i in word:
-        if(i == guessLetter) and (same == False):
-            correct = True
-    
-    if correct == True:
-        indices = [i for i, x in enumerate(word) if x == guessLetter]
+while True:
+    print("-" * 40)
+    letter = input("Player 2, guess a letter: ")
 
-        for u in range(0, len(indices)):
-            guessList.insert(indices[u], guessLetter)
-            guessList.pop(indices[u] + 1)
+    if letter.upper() in already_guessed:
+        letter = ""
+        print("Player 2, you already guessed this letter")
 
+    elif letter.upper() in word:
+        already_guessed.append(letter.upper())
+        index = word.index(letter.upper())
+        guessed[index] = letter.upper()
+        word[index] = "_"
+        print("".join(guessed))
+
+    elif letter.upper() not in word:
+        already_guessed.append(letter.upper())
+        num_guesses -= 1
+        print("Wrong! Guesses left: " + str(num_guesses))
+        print("")
+        draw(num_guesses)
+        print("")
+
+        if num_guesses == 0:
+            print("Player 2, you ran out of guesses. You lose!")
+            break            
         
-        for y in range(0, len(guessList)):
-            print(guessList[y], end=" ")
-
-            
-    elif(same == False):
-        print("Guess again!")
-    else:
-        print("You already guessed that letter")
-    
+    if "_" not in guessList:
+        print("Player 2, you won")
+        break
