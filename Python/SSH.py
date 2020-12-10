9da8c9bff78e927ddf4e9ce7aee1c73daa14e19d
Lukas <lhange42@charlottesvilleschools.org>
2020-12-10 18:49:19 +0000

    commented on code

--- Python/SSH.py
+++ Python/SSH.py
@@ -2,11 +2,11 @@ import RPi.GPIO as GPIO
 import time
 GPIO.setmode(GPIO.BCM)
 #GPIO.setwarnings(False)
-GPIO.setup(3,GPIO.OUT)
+GPIO.setup(3,GPIO.OUT)# powers that pin
 
 while True:
     print("working")
-    GPIO.output(3,GPIO.HIGH)
-    time.sleep(1)
-    GPIO.output(3,GPIO.LOW)
-    time.sleep(1)
+    GPIO.output(3,GPIO.HIGH)# turns the LED off
+    time.sleep(1)# waits 1 second
+    GPIO.output(3,GPIO.LOW)# turns the LED off
+    time.sleep(1)# waits 1 second
