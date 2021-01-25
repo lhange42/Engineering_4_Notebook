e34a693209f034ae604f91590fe84553e19e117f
Lukas <lhange42@charlottesvilleschools.org>
2021-01-25 17:45:59 +0000

    tried to find out why ellipse isn't printing

--- Python/Headless.py
+++ Python/Headless.py
@@ -31,22 +31,24 @@ timeVar = 0
 radius = 5
 
 while True:
-	if(timeVar > width)
-		draw.rectangle`((0, 0, width, height), outline=0, fill=0)
+	if timeVar > width:
+		draw.rectangle((0, 0, width, height), outline=0, fill=0)
 		timeVar = 0
 		
-	if(timeVar == 0)
+
+	if timeVar == 0:
 		draw.rectangle((0+padding, height-padding, width+padding, -height-padding), outline=255, fill=0)
-		
+		print("resets t")
 		
 	acc, mag = accelerometer.read() 
 	acc_x, acc_y, acc_z = acc # this gives me all my acceleration data even though I only need x acceleration I have to store all of them.
 	mag_x, mag_y, mag_z = mag
 	xAcc = abs(acc_x*.1) + padding # sets up the x acceleration data into data values for graphing
 	
-	draw.ellipse((timeVar,xAcc,1,1), outline = 255, fill = 255)# will draw a dot at each point and the points will be charted on the graph 
-	
-	time.sleep(1)
+	draw.ellipse((timeVar,height-xAcc,50,50), outline = 255, fill = 255)# will draw a dot at each point and the points will be charted on the graph 
+	print(xAcc)
+	time.sleep(.5)
+	timeVar = timeVar + 1
 	
 	disp.image(image)
 	disp.display()
