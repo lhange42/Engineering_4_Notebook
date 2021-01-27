bdc8f9f2926e1931f7b4016322874d28d0a214d6
Lukas <lhange42@charlottesvilleschools.org>
2021-01-27 01:59:45 +0000

    did GPIO Pins Python assignment

--- Python/GPIO_Pins_Python.py
+++ Python/GPIO_Pins_Python.py
@@ -1,2 +1,13 @@
 import RPi.GPIO as GPIO
-from time import sleep
+import time
+
+GPIO.setwarnings(False) #just gets rid of weird warnings
+leds = [15, 16] #sets up a list that contains the two pins for the LED
+GPIO.setmode(GPIO.BOARD)# makes the gpio pins identified by their board values instead of BCM values
+GPIO.setup(leds, GPIO.OUT) # makes the pins output 
+
+for i in range(0, 10): # makes a for loop that runs ten times
+	GPIO.output(leds, 1)# makes the leds turn on
+	time.sleep(0.5)# time delay 
+	GPIO.output(leds, 0)# makes the leds turn off
+	time.sleep(0.5)# time delay
