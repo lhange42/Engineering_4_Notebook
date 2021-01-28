02938e0c46b2ef2e5e424b89a20815576268d086
Lukas <lhange42@charlottesvilleschools.org>
2021-01-28 19:15:56 +0000

    working on project 2 of copy pasta

---
+++ Pictures/Copypasta_Project_2.py
@@ -0,0 +1,17 @@
+import time
+import picamera
+frame = 1
+
+with picamera.PiCamera() as camera:
+	camera.resolution = (1024, 768)
+	while True:
+		i = str(input("Click enter to take a frame or x then enter to exit: "))
+		try:
+			if i == "":
+				camera.capture('/home/pi/Documents/Engineering_4_Notebook/Pictures/Animation/frame%03d.jpg' % frame)
+				frame += 1
+			elif i == "x":
+				break
+		except KeyboardInterrupt:
+			camera.stop_preview()
+			break
